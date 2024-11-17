2f699c988f482753fc3734c8f4a030386050769e
a93942244 <781592346@qq.com>
2024-11-16 23:21:05 -0800

    fixed minimum known bugs to run the baseline

--- main.cu
+++ main.cu
@@ -31,59 +31,6 @@ void notify_renderer_exit(){
     mainRendererComm.stop_render.store(true);
 }
 
-void cornell_box() {
-    hittable_list world{1000};
-    std::vector<material*> objects;
-
-    auto red   = new lambertian(color(.65, .05, .05));
-    auto white = new lambertian(color(.73, .73, .73));
-    auto green = new lambertian(color(.12, .45, .15));
-    auto light = new diffuse_light(color(15, 15, 15));
-    objects.push_back(red);
-    objects.push_back(white);
-    objects.push_back(green);
-    objects.push_back(light);
-    auto quad_1 = new quad(point3(555,0,0), vec3(0,555,0), vec3(0,0,555), green);
-    auto quad_2 = new quad(point3(0,0,0), vec3(0,555,0), vec3(0,0,555), red);
-    auto quad_3 = new quad(point3(343, 554, 332), vec3(-130,0,0), vec3(0,0,-105), light);
-    auto quad_4 = new quad(point3(0,0,0), vec3(555,0,0), vec3(0,0,555), white);
-    auto quad_5 = new quad(point3(555,555,555), vec3(-555,0,0), vec3(0,0,-555), white);
-    auto quad_6 = new quad(point3(0,0,555), vec3(555,0,0), vec3(0,555,0), white);
-
-    world.add(quad_1);
-    world.add(quad_2);
-    world.add(quad_3);
-    world.add(quad_4);
-    world.add(quad_5);
-    world.add(quad_6);
-
-    hittable_list* box1 = create_box(point3(0,0,0), point3(165,330,165), white);
-    auto box1_rotate = new rotate_y(box1,15);
-    auto box1_translate = new translate(box1_rotate, vec3(265,0,295));
-    world.add(box1_translate);
-
-    hittable_list* box2 = create_box(point3(0,0,0), point3(165,165,165), white);
-    auto box2_rotate = new rotate_y(box2, -18);
-    auto box2_translate = new translate(box2_rotate, vec3(130,0,65));
-    world.add(box2_translate);
-
-    camera cam;
-
-    cam.aspect_ratio      = 1.0;
-    cam.image_width       = 600;
-    cam.samples_per_pixel = 200;
-    cam.max_depth         = 50;
-    cam.background        = color(0,0,0);
-
-    cam.vfov     = 40;
-    cam.lookfrom = point3(278, 278, -800);
-    cam.lookat   = point3(278, 278, 0);
-    cam.vup      = vec3(0,1,0);
-
-    cam.render(world);
-}
-
-
 hittable_list final_scene_build() {
     auto ground = new lambertian(color(0.48, 0.83, 0.53));
 
@@ -228,11 +175,15 @@ void render_thread(camera &cam, const hittable_list &scene, std::span<unsigned i
     }
 }
 
-__global__ void camera_render_cuda(camera* cam, hittable_list** scenepptr, std::span<unsigned int> image, curandState* devStates) {
-    auto tid = blockIdx.x * blockDim.x + threadIdx.x;
+__global__ void camera_init_cuda(camera* cam) {
+    const auto tid = blockIdx.x * blockDim.x + threadIdx.x;
     if (tid==0) {
         cam->initialize();
     }
+}
+
+__global__ void camera_render_cuda(camera* cam, hittable_list** scenepptr, std::span<unsigned int> image, curandState* devStates) {
+    auto tid = blockIdx.x * blockDim.x + threadIdx.x;
     auto gridSize = blockDim.x * gridDim.x;
     auto width = cam->image_width;
     auto scene = *scenepptr;
@@ -254,7 +205,7 @@ void render_thread_cuda(const camera& cam, camera* cam_cuda, hittable_list** sce
     std::span<unsigned int> imageGpu{imageGpuPtr, static_cast<std::span<unsigned>::size_type>(height*width)};
     while (!mainRendererComm.stop_render.load()) {
         if (mainRendererComm.frame_start_render.try_acquire_for(std::chrono::milliseconds(5))) {
-            //cam.render_parallel(scene, image);
+            camera_init_cuda<<<1,1>>>(cam_cuda);
             camera_render_cuda<<<GRIDDIM_X,BLOCKDIM_X>>>(cam_cuda, scene_cuda, imageGpu, devStates);
             cudaDeviceSynchronize();
             cudaMemcpy(image.data(), imageGpuPtr, image.size()*sizeof(unsigned int), cudaMemcpyDeviceToHost);
@@ -300,7 +251,7 @@ void render_scene_realtime(hittable_list &scene, camera &cam) {
     render_finished_future.wait();
 }
 
-void render_scene_realtime_cuda(hittable_list** scene, camera &cam, camera *cam_cuda, curandState* devStates) {
+void render_scene_realtime_cuda(hittable_list** scene, camera &cam, camera *cam_cuda, curandState* devStates, const int& max_frame) {
     int height = int(cam.image_width/cam.aspect_ratio);
     auto window = sdl_raii::Window{"RT_project", cam.image_width, height};
     auto renderer = sdl_raii::Renderer{window.get()};
@@ -313,8 +264,9 @@ void render_scene_realtime_cuda(hittable_list** scene, camera &cam, camera *cam_
         render_finished.set_value_at_thread_exit();
     }}.detach();
     mainRendererComm.frame_start_render.release();
+    size_t frames = 0;
     auto t0 = std::chrono::steady_clock::now();
-    while (!want_exit_sdl())
+    while (!want_exit_sdl() && ((frames < max_frame) || (max_frame < 0)))
     {
         if (mainRendererComm.frame_rendered.try_acquire_for(std::chrono::milliseconds{5})) {
             auto texture = sdl_raii::Texture{renderer.get(), surface.get()};
@@ -322,6 +274,7 @@ void render_scene_realtime_cuda(hittable_list** scene, camera &cam, camera *cam_
             SDL_RenderCopy(renderer.get(),texture.get(), nullptr, nullptr);
             SDL_RenderPresent(renderer.get());
             auto frame_time = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now()-t0);
+            frames += 1;
             utils::log("Frame time: "+std::to_string(frame_time.count()/1e3)+" ms");
             mainRendererComm.frame_start_render.release();
             t0 = std::chrono::steady_clock::now();
@@ -338,9 +291,35 @@ __global__ void initCurand(curandState *state, unsigned long seed){
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
     curand_init(seed, idx, 0, &state[idx]);
 }
+
+void parse_arguments(int argc, char** argv, int& size, int& samples, int& depth, int& frame) {
+    for (int i = 1; i < argc; i += 2) {
+        if (std::string(argv[i]) == "--size") {
+            size = std::atoi(argv[i + 1]);
+        } else if (std::string(argv[i]) == "--samples") {
+            samples = std::atoi(argv[i + 1]);
+        } else if (std::string(argv[i]) == "--depth") {
+            depth = std::atoi(argv[i + 1]);
+        } else if (std::string(argv[i]) == "--frame") {
+            frame = std::atoi(argv[i + 1]);
+        } else {
+            std::cerr << "Usage: " << argv[0]
+                << " --size <int> --depth <int> --samples <int> --device <string>\n\n"
+                   "       --size: width of image in px\n"
+                   "       --depth: maximum depth for rays\n"
+                   "       --samples: number of samples per pixel\n"
+                   "       --frame: non-stop when set to negative\n" << std::endl;
+        }
+    }
+}
+
 int main(int argc, char* argv[]) {
     sdl_raii::SDL sdl{};
     initialize_main_sync_objs();
+
+    int size = 400, samples = 32, depth = 3, frame = 62;
+    parse_arguments(argc, argv, size, samples, depth, frame);
+
     curandState *devStates;
     cudaMalloc(&devStates, GRIDDIM_X*BLOCKDIM_X*sizeof(curandState));
     initCurand<<<GRIDDIM_X,BLOCKDIM_X>>>(devStates, 1);
@@ -352,17 +331,11 @@ int main(int argc, char* argv[]) {
     cudaDeviceSynchronize();
     utils::cu_check();
     // FIXME: camera::ray_color() causes stackoverflow on CUDA
-    final_camera_cuda<<<1,1>>>(400, 50, 3, camGpuPtr);
+    final_camera_cuda<<<1,1>>>(size, samples, depth, camGpuPtr);
     cudaDeviceSynchronize();
     utils::cu_check();
-    auto scene = debug_scene_build(nullptr);
-    auto cam = final_camera(400, 50, 4);
-    if (argc!=1) {
-        render_scene(scene, cam);
-    } else {
-        //render_scene_realtime(scene, cam);
-        render_scene_realtime_cuda(sceneGpuPtr, cam, camGpuPtr, devStates);
-    }
+    auto cam = final_camera(size, samples, depth);
+    render_scene_realtime_cuda(sceneGpuPtr, cam, camGpuPtr, devStates, frame);
     cudaFree(sceneGpuPtr);
     cudaFree(camGpuPtr);
     cudaFree(devStates);
